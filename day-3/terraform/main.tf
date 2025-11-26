4b914a2578ca826d585c83a96a795109c539a2ec
ukduw <edhkmayo@gmail.com>
2025-11-26 09:40:38 -0500

    added provider block

---
+++ day-3/terraform/main.tf
@@ -0,0 +1,11 @@
+# Create an EC2 instance
+
+# Provider block - what provider(s) to interact with
+provider "aws" {
+    region = "eu-west-1"
+
+    # Terraform will download all required dependencies/plugins when `terraform init` is run
+}
+
+
+
