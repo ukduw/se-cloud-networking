709cf648e544996b0c65ae8fef6c129389bc7088
ukduw <edhkmayo@gmail.com>
2025-11-26 10:05:11 -0500

    added service/resource block

--- day-3/terraform/main.tf
+++ day-3/terraform/main.tf
@@ -7,5 +7,17 @@ provider "aws" {
     # Terraform will download all required dependencies/plugins when `terraform init` is run
 }
 
+# Service/resource
+resource "aws_instance" "basic_instance" {
+    ami = "ami-0b985cf5be254fc1a"
+    instance_type = "t3.micro"
+    associate_public_ip_address = true
+
+    # Name instance on AWS
+    tags = {
+        Name = "se-edmund-test-tf-instance"
+    }
+}
+
 
 
