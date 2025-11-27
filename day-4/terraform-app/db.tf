f587d2def8f6f42895382cfab998ae319661eaff
ukduw <edhkmayo@gmail.com>
2025-11-27 06:19:27 -0500

    re-labeled, commented out provider since it only needs to be configured once on initialization

--- day-4/terraform-app/db.tf
+++ day-4/terraform-app/db.tf
@@ -1,9 +1,9 @@
-provider "aws" {
-    region = "eu-west-1"
-}
+#provider "aws" {
+#    region = "eu-west-1"
+#}
 
 # Service/resource
-resource "aws_instance" "basic_instance" {
+resource "aws_instance" "db_instance" {
     ami = "ami-068be8a2a1c4f493f"
     instance_type = "t3.micro"
     associate_public_ip_address = false
