54cb1c135065ba5f9f2f0807018f7b1a9b8db41a
ukduw <edhkmayo@gmail.com>
2025-11-27 11:09:44 -0500

    parameterized region/az, cidr blocks, TODO: parameterize ports...

--- day-4/terraform-app/app-db.tf
+++ day-4/terraform-app/app-db.tf
@@ -1,12 +1,12 @@
 # Provider block - what provider(s) to interact with
 provider "aws" {
-    region = "eu-west-1"
+    region = var.aws_region
 }
 
 # Service/resource blocks
 # === VPC ===
 resource "aws_vpc" "main" {
-    cidr_block          = "10.0.0.0/16"
+    cidr_block          = var.vpc_cidr
     enable_dns_support  = true
     enable_dns_hostnames= true
 
@@ -19,8 +19,8 @@ resource "aws_vpc" "main" {
 # === Subnets ===
 resource "aws_subnet" "subnet_private" {
     vpc_id              = aws_vpc.main.id
-    cidr_block          = "10.0.2.0/24"
-    availability_zone   = "eu-west-1a"
+    cidr_block          = var.priv_subnet_cidr
+    availability_zone   = var.aws_az_a
 
     tags = {
         Name = "se-edmund-tf-priv-subnet"
@@ -29,8 +29,8 @@ resource "aws_subnet" "subnet_private" {
 
 resource "aws_subnet" "subnet_public" {
     vpc_id              = aws_vpc.main.id
-    cidr_block          = "10.0.3.0/24"
-    availability_zone   = "eu-west-1b"
+    cidr_block          = var.pub_subnet_cidr
+    availability_zone   = var.aws_az_b
 
     tags = {
         Name = "se-edmund-tf-pub-subnet"
