65b2ecd7a29b98ac6c28f787e12406458e6c54cc
ukduw <edhkmayo@gmail.com>
2025-11-27 07:26:28 -0500

    moved db to app, renamed tf

--- day-4/terraform-app/app-db.tf
+++ day-4/terraform-app/app-db.tf
@@ -4,6 +4,20 @@ provider "aws" {
 }
 
 # Service/resource
+resource "aws_instance" "db_instance" {
+    ami = "ami-068be8a2a1c4f493f"
+    instance_type = "t3.micro"
+    associate_public_ip_address = false
+    security_groups = ["se-edmund-mongodb-sg"]
+
+    key_name = "se-edmund-key-pair"
+
+    # Name instance on AWS
+    tags = {
+        Name = "se-edmund-tf-db"
+    }
+}
+
 resource "aws_instance" "app_instance" {
     ami = "ami-0b985cf5be254fc1a"
     instance_type = "t3.micro"
