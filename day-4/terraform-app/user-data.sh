faf758fd853657ff0bffd1f373df41272070fb6f
ukduw <edhkmayo@gmail.com>
2025-11-27 06:19:46 -0500

    added user data script; no idea how to reference the db's private ip

---
+++ day-4/terraform-app/user-data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sleep 15
+
+export DB_HOST=mongodb://<DB-IPADDRESS>:27017/posts
+cd /home/ubuntu
+cd se-test-app/nodejs20-se-test-app-2025/app
+
+sudo npm install
+node seeds/seed.js
+
+pm2 start app.js
