fb8202fe7e43ffc8fb9bd93cdb9dab1a87416ce5
ukduw <edhkmayo@gmail.com>
2025-11-27 10:47:23 -0500

    added all vpc resource blocks, other than SGs... need to add their id's to instances, need to figure out what needs to be made into variables...

--- day-4/terraform-app/app-db.tf
+++ day-4/terraform-app/app-db.tf
@@ -3,28 +3,97 @@ provider "aws" {
     region = "eu-west-1"
 }
 
-# Service/resource
+# Service/resource blocks
+# === VPC ===
+resource "aws_vpc" "main" {
+    cidr_block          = "10.0.0.0/16"
+    enable_dns_support  = true
+    enable_dns_hostnames= true
+
+    tags = {
+        Name = "se-edmund-tf-vpc"
+    }
+}
+
+# === Subnets ===
+resource "aws_subnet" "subnet_private" {
+    vpc_id              = aws_vpc.main.id
+    cidr_block          = "10.0.2.0/24"
+    availability_zone   = "eu-west-1a"
+
+    tags = {
+        Name = "se-edmund-tf-priv-subnet"
+    }
+}
+
+resource "aws_subnet" "subnet_public" {
+    vpc_id              = aws_vpc.main.id
+    cidr_block          = "10.0.3.0/24"
+    availability_zone   = "eu-west-1b"
+
+    tags = {
+        Name = "se-edmund-tf-pub-subnet"
+    }
+}
+
+# === IGW ===
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.main.id
+
+    tags = {
+        Name = "se-edmund-tf-igw"
+    }
+}
+
+# === RT ===
+resource "aws_route_table" "public_rt" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
+    }
+
+    tags = {
+        Name = "se-edmund-tf-rt"
+    }
+}
+
+# === RT association ===
+resource "aws_route_table_association" "public_association" {
+    subnet_id       = aws_subnet.subnet_public.id
+    route_table_id  = aws_route_table.public_rt.id
+}
+
+# === SGs ===
+resource "aws_security_group" "db_sg" {
+
+}
+
+
+# === EC2 instances ===
 resource "aws_instance" "db_instance" {
-    ami = var.db_ami_id
-    instance_type = var.db_instance_type
+    ami                         = var.db_ami_id
+    instance_type               = var.db_instance_type
     associate_public_ip_address = false
-    security_groups = [var.db_sg]
+    subnet_id                   = aws_subnet.subnet_private.id
+    security_groups             = []
 
-    key_name = var.db_key_pair
+    key_name                    = var.db_key_pair
 
-    # Name instance on AWS
     tags = {
         Name = "se-edmund-tf-db"
     }
 }
 
 resource "aws_instance" "app_instance" {
-    ami = var.app_ami_id
-    instance_type = var.instance_type
+    ami                         = var.app_ami_id
+    instance_type               = var.instance_type
     associate_public_ip_address = true
-    security_groups = [var.app_sg]
+    subnet_id                   = aws_subnet.subnet_public.id
+    security_groups             = []
 
-    key_name = var.app_key_pair
+    key_name                    = var.app_key_pair
 
     user_data = templatefile("user-data.sh", {
         DATABASE_IP = aws_instance.db_instance.public_ip
@@ -33,7 +102,6 @@ resource "aws_instance" "app_instance" {
     # Ensure db is created first
     depends_on = [aws_instance.db_instance]
 
-    # Name instance on AWS
     tags = {
         Name = "se-edmund-tf-app"
     }
