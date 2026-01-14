9aef496423276995e0f11062b0a3ec9303eab83b
ukduw <edhkmayo@gmail.com>
2026-01-14 10:56:00 -0500

    fixed region, added account id, arn fetch

--- day-x-1-ansible/terraform-reintro/main.tf
+++ day-x-1-ansible/terraform-reintro/main.tf
@@ -1,3 +1,14 @@
 provider "aws" {
-    region = eu-west-1
+    region = "eu-west-1"
 }
+
+data "aws_caller_identity" "current" {}
+
+output "aws_account_id" {
+    value = data.aws_caller_identity.current.account_id
+}
+
+output "aws_user_arn" {
+    value = data.aws_caller_identity.current.arn
+}
+
